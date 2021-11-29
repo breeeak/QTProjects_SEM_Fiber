013adb28d7335ee0ab087679a7cfb15b1d7815e0
breeeak <1060913206@qq.com>
2021-11-30 00:22:24 +0800

    更新 可以导出直径

--- utils.py
+++ utils.py
@@ -49,13 +49,15 @@ def batch_export(root, out_dir="out", configs=None):
 
     workbook = xlwt.Workbook()  # 新建一个工作簿
     sheet = workbook.add_sheet("检测值")  # 在工作簿中新建一个表格
+    sheet2 = workbook.add_sheet("直径分布")  # 在工作簿中新建一个表格
     if configs.out_list == "ALL":
         sheet.write(0, 0, "文件名")
-        sheet.write(0, 1, "平均直径")
+        sheet.write(0, 1, "平均直径um")
         sheet.write(0, 2, "平均直径(像素)")
         sheet.write(0, 3, "直径cv")
         sheet.write(0, 4, "孔隙率")
         sheet.write(0, 5, "孔隙cv")
+        sheet2.write(0, 0, "直径值um")
     else:
         pass
     i = 1
@@ -70,7 +72,7 @@ def batch_export(root, out_dir="out", configs=None):
             img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
             scale = configs.scale
         reults_dic, reults_fig = sem_analysis(img)
-        diameter, diameter_cv, porosity, porosity_cv = reults_dic["diameter"],reults_dic["diameter_cv"],reults_dic["porosity"],reults_dic["porosity_cv"]
+        diameter, diameter_cv, porosity, porosity_cv, D = reults_dic["diameter"],reults_dic["diameter_cv"],reults_dic["porosity"],reults_dic["porosity_cv"],reults_dic["D"]
         # cv2.imwrite(os.path.join(out_dir, file_path), img)
         (filepath, tempfilename) = os.path.split(img_path)
         file_name = os.path.splitext(tempfilename)[0]
@@ -91,8 +93,11 @@ def batch_export(root, out_dir="out", configs=None):
         sheet.write(i, 4, porosity)
         sheet.write(i, 5, porosity_cv)
         i = i + 1
+        # 存储D
+        for m in range(len(D)):
+            sheet2.write(m+1,0,D[m])
         print("###" * 10)
-        print(file_path, "平均直径:", real_diameter)
+        print(file_path, "平均直径um:", real_diameter)
         print(file_path, "直径cv:", diameter_cv)
         print(file_path, "孔隙率:", porosity)
         print(file_path, "孔隙cv:", porosity_cv)
@@ -163,15 +168,18 @@ def sem_analysis(img):
         if cv2.contourArea(contour) < h * w * 0.5:  # 面积阈值
             areas.append(cv2.contourArea(contour))
     porosity_cv = np.std(areas) / np.mean(areas)
-    reults_dic = {"diameter":diameter, "diameter_cv":diameter_cv, "porosity":porosity, "porosity_cv":porosity_cv}
+    reults_dic = {"diameter":diameter, "diameter_cv":diameter_cv, "porosity":porosity, "porosity_cv":porosity_cv,"D": D}
     reults_fig = {"th2":th2, "dilated":dilated, "D":dist_fig}
     return reults_dic, reults_fig
 
 
 if __name__ == '__main__':
-    imgP = "./data/logo.jpg"
-    re = threshold_Img(imgP)
-    # 显示图片
-    cv2.imshow('girl', re)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imgP = "./data/2_002.tif"
+    batch_export(imgP)
+
+
+    # re = threshold_Img(imgP)
+    # # 显示图片
+    # cv2.imshow('girl', re)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
